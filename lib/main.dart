b8f77bd9a9c945107c8314c69ba7cb1af8849027
jekapv <160306841+jekapv@users.noreply.github.com>
2025-10-18 08:14:51 +0000

    feat: add interactive elements

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Лабораторная №1')),
-        body: Column(
-          children: [
-            Container(width: 200, height: 100, color: Colors.red),
-            Row(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Лабораторная №1')),
+      body: Column(
+        children: [
+          Container(width: 200, height: 100, color: Colors.red),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Text('Текст 1'),
+              Text('Текст 2'),
+              Text('Текст 3'),
+            ],
+          ),
+          Container(width: 150, height: 80, color: Colors.green),
+          Expanded(
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Text('Текст 1'),
-                Text('Текст 2'),
-                Text('Текст 3'),
+              children: [
+                CircleAvatar(radius: 40, backgroundColor: Colors.blue),
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                    'https://docs.flutter.dev/assets/images/shared/brand/flutter/logo/flutter-lockup.png',
+                  ),
+                ),
               ],
             ),
-            Container(width: 150, height: 80, color: Colors.green),
-          ],
-        ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Button pressed!');
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
